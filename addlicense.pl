3b95b658eb9e7302b88a04201beba0710ccd6c60
Michael Wallio <michael.wallio@outlook.com>
2013-06-06 22:57:18 -0400

    Removed debugging printing statements.

--- addlicense.pl
+++ addlicense.pl
@@ -45,9 +45,7 @@ my $PATCH = 0;
 sub get_licenses
 {
   my @licenses;
-  print "${Bin}\n";
   my $license_dir = File::Spec->catfile("${Bin}", "license_files");
-  print "${license_dir}\n";
   opendir(DIRHANDLE, $license_dir) or die $!;
   while (my $file = readdir(DIRHANDLE)) {
     my $file_path = File::Spec->catfile($license_dir, $file);
